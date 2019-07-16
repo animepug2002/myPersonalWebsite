5f48910352e20c17914e38894a5612990ac98aa8
Jewel Wannop <48057552+animepug2002@users.noreply.github.com>
2019-07-16 15:49:44 -0400

    Add files via upload

---
+++ myJavaScript.js
@@ -0,0 +1,11 @@
+alert("Are you ready to go on a magical adventure to the inside of Jewel's brain.");
+
+function changecolor(color, id){
+  var element = document.getElementById(id);
+  element.style.color = color;
+}
+
+function showcontent(id){
+  var element = document.getElementById(id);
+  element.style.display = "blank";
+}
